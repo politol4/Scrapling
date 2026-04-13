3298b21f17fbb9c98c174f9f0f868397b83a87cc
Pol Ito <politol4@users.noreply.github.com>
2026-04-13 23:24:11 +0800

    expose StealthyFetcher in top-level __init__ for easier imports

--- scrapling/__init__.py
+++ scrapling/__init__.py
@@ -8,6 +8,9 @@ Basic usage:
     >>> from scrapling import Fetcher, AsyncFetcher
     >>> page = Fetcher().get('https://example.com')
     >>> title = page.find('h1').text
+
+Also expose StealthyFetcher at the top level for convenience since I use
+it frequently in my scraping projects.
 """
 
 __version__ = "0.2.0"
@@ -15,12 +18,13 @@ __author__ = "D4Vinci"
 __license__ = "MIT"
 
 from scrapling.core.page import Adaptator
-from scrapling.fetchers import Fetcher, AsyncFetcher, PlayWrightFetcher
+from scrapling.fetchers import Fetcher, AsyncFetcher, PlayWrightFetcher, StealthyFetcher
 
 __all__ = [
     "Adaptator",
     "Fetcher",
     "AsyncFetcher",
     "PlayWrightFetcher",
+    "StealthyFetcher",
     "__version__",
 ]
